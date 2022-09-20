13620e44553da60f1749285c3ad084555f0e32eb
jhowlett-scottlogic <jhowlett@scottlogic.com>
2022-09-20 15:59:59 +0100

    change slashes

--- test.js
+++ test.js
@@ -10,4 +10,4 @@ const featurePath = clArgs[0] || "../openapi-forge/features/*.feature";
 const generatePath = clArgs[1] || "openapi-forge/src/generate";
 
 // Pass both paths to cucumber-js which spawns another node process to handle the testing.
-shell.exec(`.\\node_modules\\.bin\\cucumber-js -p default ${featurePath} ${generatePath}`);
+shell.exec(`./node_modules/.bin/cucumber-js -p default ${featurePath} ${generatePath}`);
