f2d5f3e2e4972a94416bf83b0577ae6f627076a1
jhowlett-scottlogic <jhowlett@scottlogic.com>
2022-09-21 10:30:47 +0100

    wwdw

--- test.js
+++ test.js
@@ -10,5 +10,4 @@ const featurePath = clArgs[0] || "node_modules/openapi-forge/features/*.feature"
 const generatePath = clArgs[1] || "openapi-forge/src/generate";
 
 // Pass both paths to cucumber-js which spawns another node process to handle the testing.
-shell.cd("./node_modules/.bin");
-shell.exec(`cucumber-js -p default "${featurePath}" "${generatePath}"`);
+shell.exec(`node_modules/.bin/cucumber-js -p default "${featurePath}" "${generatePath}"`);
