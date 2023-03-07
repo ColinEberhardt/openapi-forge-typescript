c9932544c18ac65a73f0169f0740830a60b39fdc
Colin Eberhardt <colin.eberhardt@gmail.com>
2023-03-07 14:59:19 +0000

    feat: added response headers

--- template/response.ts
+++ template/response.ts
@@ -2,8 +2,27 @@
  * The response of a HTTP request, contains both the response data and associated metadata.
  */
 export interface HttpResponse<T> {
+  /**
+   * The response data.
+   * @type T
+   */
   data: T;
+
+  /**
+   * The response status code.
+   * @type number
+   */
   statusCode: number;
+
+  /**
+   * The response headers.
+   * @type Record<string, string>
+   */
   headers: Record<string, string>;
+
+  /**
+   * The type of the response (e.g., basic, cors).
+   * @type string
+   */ 
   type: string;
 }
